40d557e9182c374060a8d8361fce92ada3b875d7
mcgear <michael.gearhardt@gmail.com>
2022-06-17 16:25:38 -0600

    not sure what was messed up with this

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const PackageJsonDeploy = require('./src/lib/PackageJsonDeploy.js');
 
 module.exports = merge(common, {
   output: {
